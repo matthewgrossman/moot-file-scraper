34b5df33d2e4149086cffd84c5b891a9cced92fb
matthewgrossman <matt@mrgrossman.com>
2015-05-30 20:20:21 +0100

    Finished get_tims func

--- scraper.py
+++ scraper.py
@@ -21,35 +21,28 @@ def rate_limit(limit):
         return wrapped_f
     return wrap
 
+# get all thread_ids for a given board
 def thread_ids(boardname):
     response = urllib.request.urlopen("http://a.4cdn.org/"+ boardname + "/threads.json")
     data = json.loads(response.read().decode())
 
     return [thread["no"] for page in data for thread in page["threads"]]
-    
-    # list comprehensions are pretty, but the code is doing this:
-    #thread_ids = []
-    #for page in data:
-    #    for thread in page["threads"]:
-    #        thread_ids.append(thread["no"])
-    #return thread_ids
 
-# get urls of all gifs in a given thread
+# get tims of all gifs in a given thread
 def gif_urls_for_thread(boardname, thread_id):
-    response = urllib.request.urlopen("http://a.4cdn.org/" + boardname+ "/thread/" + thread_id + ".json")
+    response = urllib.request.urlopen("http://a.4cdn.org/" + boardname+ "/thread/" + str(thread_id) + ".json")
     data = json.loads(response.read().decode())
 
-    # parse the JSON and add all the gif urls to the array
-    gif_urls = []
-    print(data['posts'][0]['now'])
+    # use post.get(key) because not all posts have a 'ext' attribute
+    return [post['tim'] for post in data['posts'] if post.get('ext') == '.gif' ]
 
-    return gif_urls
-
+# download a gif given its tim and board name at a specified path
 def download_gif(gif_tim, boardname, save_path):
     out_path = save_path + boardname + '_' + str(gif_tim)
     gif_url = "http://i.4cdn.org/" + boardname + "/" + str(gif_tim) + ".gif"
-    print(gif_url)
+
     with urllib.request.urlopen(gif_url) as response, open(out_path, 'wb') as out_file:
         out_file.write(response.read())
 
-download_gif(1432997104636, 'vg', options.dest) 
+print(gif_urls_for_thread('vg', 104844212))
+#download_gif(1432997104636, 'vg', options.dest) 
