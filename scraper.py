2b0c31df2d0a5915766054ff9591af7decea7789
matthewgrossman <matt@mrgrossman.com>
2015-05-30 17:01:22 +0100

    Added function to get threads for a given boardjj:w

--- scraper.py
+++ scraper.py
@@ -1,3 +1,7 @@
+import pprint
+import urllib.request
+import json
+
 from optparse import OptionParser
 
 parser = OptionParser()
@@ -5,3 +9,13 @@ parser.add_option("-b", "--board", type="string", dest="boardname", default="b")
 parser.add_option("-d", "--dest", type="string", dest="dest", default="")
 (options, args) = parser.parse_args()
 
+# makes one request
+def thread_ids(boardname):
+    response = urllib.request.urlopen("http://a.4cdn.org/"+ boardname + "/threads.json")
+    data = json.loads(response.read().decode())
+
+    thread_ids = []
+    for page in data:
+        for thread in page["threads"]:
+            thread_ids.append(thread["no"])
+    return thread_ids
