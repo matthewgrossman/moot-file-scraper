2a2e471eb10039935d2bec467af53ae0dedd94a5
matthewgrossman <matt@mrgrossman.com>
2015-05-30 16:33:44 +0100

    Added default args

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option("-b", "--board", type="string", dest="boardname")
-parser.add_option("-d", "--dest", type="string", dest="dest")
+parser.add_option("-b", "--board", type="string", dest="boardname", default="b")
+parser.add_option("-d", "--dest", type="string", dest="dest", default="")
 (options, args) = parser.parse_args()
 
