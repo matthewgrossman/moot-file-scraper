f84461c9c95dcfeb1669c787af8810c487f91c8b
matthewgrossman <matt@mrgrossman.com>
2015-05-30 17:37:14 +0100

    Added rate limit decorator

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-import pprint
+import time
 import urllib.request
 import json
 
@@ -9,7 +9,16 @@ parser.add_option("-b", "--board", type="string", dest="boardname", default="b")
 parser.add_option("-d", "--dest", type="string", dest="dest", default="")
 (options, args) = parser.parse_args()
 
-# makes one request
+# sleeps for time=limit before calling the function
+def rate_limit(limit):
+    def wrap(f):
+        def wrapped_f(*args):
+            time.sleep(limit)
+            f(*args)
+        return wrapped_f
+    return wrap
+
+@rate_limit(1.0)
 def thread_ids(boardname):
     response = urllib.request.urlopen("http://a.4cdn.org/"+ boardname + "/threads.json")
     data = json.loads(response.read().decode())
@@ -23,4 +32,6 @@ def thread_ids(boardname):
     #        thread_ids.append(thread["no"])
     #return thread_ids
 
-print(thread_ids('pol'))
+# def gif_urls_for_threads(thread_ids):
+#     for thread in thread_ids:
+
