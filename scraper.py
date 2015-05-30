ab5855158cb53a1f4e76154aef6e6bb8ee766a0f
matthewgrossman <matt@mrgrossman.com>
2015-05-30 18:04:15 +0100

    Added in individual gif fetching

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import os.path
 import time
 import urllib.request
 import json
@@ -8,6 +9,8 @@ parser = OptionParser()
 parser.add_option("-b", "--board", type="string", dest="boardname", default="b")
 parser.add_option("-d", "--dest", type="string", dest="dest", default="")
 (options, args) = parser.parse_args()
+
+absolute_path = os.path.join(os.getcwd(), options.dest)
 
 # sleeps for time=limit before calling the function
 def rate_limit(limit):
@@ -32,6 +35,16 @@ def thread_ids(boardname):
     #        thread_ids.append(thread["no"])
     #return thread_ids
 
+@rate_limit(1.0)
+def download_gif(gif_url, save_path):
+    out_path = save_path + '/' + gif_url.split('/')[-1]
+    with urllib.request.urlopen(gif_url) as response, open(out_path, 'wb') as out_file:
+        data = response.read()
+        out_file.write(data)
+
+download_gif("http://i.4cdn.org/b/1433004501165.gif", absolute_path)
+
+    
 # def gif_urls_for_threads(thread_ids):
 #     for thread in thread_ids:
 
